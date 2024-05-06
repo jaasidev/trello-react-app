f338fabd4e78360a35a0a6673fa2acd49b1e2731
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-05-06 13:21:50 -0400

    intercambiando footer por about

---
+++ src/components/about.jsx
@@ -0,0 +1,11 @@
+export function Footer () {
+  return (
+    <dialog>
+      <div className='card'>
+        <div className='card-body'>
+          {}
+        </div>
+      </div>
+    </dialog>
+  )
+}
--- src/components/footer.jsx
+++
@@ -1,7 +0,0 @@
-export function Footer () {
-  return (
-    <footer className='flex items-center justify-center fixed bottom-0 w-full py-5'>
-      <span className='text-md text-white'>Hecho por Jaasiel Beltrán</span>
-    </footer>
-  )
-}
