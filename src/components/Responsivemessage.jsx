f394ab7cf4d45a94344b79846ae8b0468eca56c5
Jaasiel Beltran <jaasiel0303@gmail.com>
2024-05-08 19:06:04 -0400

    poniendo un cartel responsive para las versiones de mobile

---
+++ src/components/Responsivemessage.jsx
@@ -0,0 +1,9 @@
+export function NoResponsive () {
+  return (
+    <div className='fixed top-0 right-0 left-0 bottom-0 bg-white text-[--text-100] text-2xl flex items-center justify-center lg:hidden text-center'>
+      <span>
+        No esta disponible para este tamaño de pantalla
+      </span>
+    </div>
+  )
+}
